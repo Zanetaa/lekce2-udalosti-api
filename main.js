19c5a7c95581b5d90b9225fbe05132b5873cf007
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-02-15 21:42:30 +0100

    Úpravy formátu

--- main.js
+++ main.js
@@ -2,10 +2,15 @@
 
 console.log('Ahoj, jsem nalinkovaný.');
 
+//tučné písmo
+
 function tucnePismo() {
     document.querySelector('.elipsa').style.fontWeight='bold';
 }
 
+//červený po kliknutí
+
 function changeRed() {
     document.querySelector('.elipsa').classList.toggle("cerveny-text");
 }
+

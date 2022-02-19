01e2e3bc92aa40a8c870f75259ad59da2cc197fd
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-02-19 14:04:07 +0100

    Úkol č.3

--- main.js
+++ main.js
@@ -27,6 +27,30 @@ function startAudio() {
     audioFile.play()
 }
 
+function pause() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.pause()
+}
+
+function loudZero() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.volume = 0
+}
+
+function loudZeroFive() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.volume = 0.5
+}
+
+function loudOne() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.volume = 1
+}
+
+function replay() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.currentTime = 0
+}
 
 //let zvuk;
 
